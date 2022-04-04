7167bb2c4f0030c694748ae0067707ef80a33617
KKlarme <98957591+kklarme@users.noreply.github.com>
2022-04-05 01:27:34 +0200

    :recycle: Use ServerPropConverter
    Additionally remove most of <Head> because it was moved to DefaultLayout

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
-import { Project } from 'spine-api';
 import testData from '../testData.json';
 import dynamic from 'next/dynamic';
+import { ServerProjectProp, ServerPropConverter } from '../ServerPropConverter';
+import api from '../api';
 
 export interface ProjectsPageProps {
-  projects: Project[];
+  projects: ServerProjectProp[];
 }
 
 // Render project table only on client because we need window and document for correct dimension calculation
@@ -13,45 +14,30 @@ const DynamicProjectListWithoutSSR = dynamic(() => import('../components/Project
   ssr: false,
 });
 
-const Projects: NextPage<ProjectsPageProps> = (props) => {
+const ProjectsPage: NextPage<ProjectsPageProps> = (props) => {
+  const projects = props.projects.map(ServerPropConverter.toProject);
   return (
     <div className="flex flex-col min-h-0 flex-grow">
       <Head>
-        <title>Spine Web</title>
-        <meta name="description" content="Unofficial web client for Spine" />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#871313" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#871313" />
+        <title>Spine Web - Projects</title>
       </Head>
 
       <main className="px-6 py-3 h-full">
-        <DynamicProjectListWithoutSSR projects={props.projects} />
+        <DynamicProjectListWithoutSSR projects={projects} />
       </main>
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // const projects = await SpineApi.getProjects({
-  //   serverUrl: process.env.SERVER_URL || '',
-  //   credentials: {
-  //     username: process.env.SERVER_USERNAME || '',
-  //     password: process.env.SERVER_PASSWORD || '',
-  //   },
-  // });
-  const projects = testData.slice();
+export const getServerSideProps: GetServerSideProps<ProjectsPageProps> = async (context) => {
+  // const projects = await api.getProjects();
+  // const projectProps = projects.map(ServerPropConverter.toServerProp);
+  const projectProps = testData as ServerProjectProp[];
   return {
     props: {
-      projects,
+      projects: projectProps,
     },
   };
 };
 
-export default Projects;
+export default ProjectsPage;
