6e5b5cc13072b8260b65f918166044395662664a
KKlarme <98957591+kklarme@users.noreply.github.com>
2022-04-05 01:23:27 +0200

    :recycle: Move favicon and meta head tags into DefaultLayout

--- app/src/layout/DefaultLayout.tsx
+++ app/src/layout/DefaultLayout.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import LanguageSelect from '../components/LanguageSelect';
 import { Language } from 'spine-api';
 import { useRouter } from 'next/router';
+import Head from "next/head";
 
 const DefaultLayout: FC = (props) => {
   const router = useRouter();
@@ -15,6 +16,20 @@ const DefaultLayout: FC = (props) => {
 
   return (
     <div className="flex flex-col max-h-screen h-screen">
+      <Head>
+        <meta name="description" content="Unofficial web client for Spine" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#871313" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#871313" />
+      </Head>
+
       <div className="h-14 flex items-center flex-shrink-0 justify-between px-4 bg-gray-500 text-white">
         <a
           href="https://clockwork-origins.com/de/spine/"
