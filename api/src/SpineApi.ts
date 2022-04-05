a8a6669e934560bb4ec8fdc6faf52d7bddbab845
KKlarme <98957591+kklarme@users.noreply.github.com>
2022-04-05 15:42:46 +0200

    :sparkles: Implement downloading of images from Spine server

--- api/src/SpineApi.ts
+++ api/src/SpineApi.ts
@@ -2,6 +2,7 @@ import { Credentials, RawProjectInfo, RequestAllProjectsResponse } from './types
 import axios from 'axios';
 import { Project } from './Project';
 import { ProjectInfo } from './ProjectInfo';
+import { inflateRaw } from 'pako';
 
 export interface SpineApiConfig {
   serverUrl: string;
@@ -56,6 +57,21 @@ export class SpineApi {
     return new ProjectInfo(projectInfo);
   }
 
+  static async downloadImage(url: string): Promise<Uint8Array> {
+    // the response is a buffer that contains raw deflated data
+    const response = await axios.get<Buffer>(url, {
+      responseType: 'arraybuffer',
+    });
+    // zlib seems to add two bytes to the start of the buffer that can't be handled by nodejs/pako\
+    // these two bytes are probably a header but none of the nodejs/pako methods seem to be able to handle that
+    // therefore these two bytes are simply omitted
+    const buffer = Buffer.alloc(response.data.length - 2);
+    // copy everything but the first two bytes into buffer
+    response.data.copy(buffer, 0, 2);
+    // lastly we want to decompress the buffer
+    return inflateRaw(buffer);
+  }
+
   constructor(public config: SpineApiConfig) {}
 
   get language(): string {
@@ -76,5 +92,9 @@ export class SpineApi {
 
   async getProjectInfo(id: string): Promise<ProjectInfo> {
     return SpineApi.getProjectInfo(id, this.config);
+  }
+
+  async downloadImage(url: string): Promise<Uint8Array> {
+    return SpineApi.downloadImage(url);
   }
 }
