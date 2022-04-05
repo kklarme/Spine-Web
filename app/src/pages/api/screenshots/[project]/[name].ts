80337785d6fe345e84837192170eced1a75a668e
KKlarme <98957591+kklarme@users.noreply.github.com>
2022-04-05 15:43:56 +0200

    :sparkles: Implement api route that returns the image from the Spine sever

---
+++ app/src/pages/api/screenshots/[project]/[name].ts
@@ -0,0 +1,13 @@
+import { SpineApi } from 'spine-api';
+import { NextApiHandler } from 'next';
+
+const handler: NextApiHandler = async function handler(req, res) {
+  const { project, name } = req.query;
+  if (typeof project !== 'string' || typeof name !== 'string') return res.status(401).json({});
+  const imageUrl = `https://clockwork-origins.de/Gothic/downloads/mods/${project}/screens/${name}`;
+  const fileArrayBuffer = await SpineApi.downloadImage(imageUrl);
+  const fileBuffer = new Buffer(fileArrayBuffer);
+  res.setHeader('Content-Type', `image/png`).send(fileBuffer);
+};
+
+export default handler;
