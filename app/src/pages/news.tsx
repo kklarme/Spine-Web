d58d305abf01ae7cdea427e34a321b00ed5b44a8
KKlarme <98957591+kklarme@users.noreply.github.com>
2022-04-23 01:43:57 +0200

    :recycle: Move load-overlay into component

--- app/src/pages/news.tsx
+++ app/src/pages/news.tsx
@@ -3,8 +3,9 @@ import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
 import { News } from 'spine-api';
+import LoadOverlay from '../components/LoadOverlay';
 
-const NewsPage: NextPage = (props) => {
+const NewsPage: NextPage = () => {
   const { i18n } = useTranslation();
   const [isLoading, setLoading] = useState(true);
   const [news, setNews] = useState<News[]>([]);
@@ -20,30 +21,7 @@ const NewsPage: NextPage = (props) => {
       </Head>
 
       <main className="h-full">
-        {isLoading && (
-          <div className="absolute inset-0 flex items-center justify-center z-50 bg-black bg-opacity-80">
-            <svg
-              className="animate-spin h-12 w-12 text-accent-light"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-          </div>
-        )}
+        {isLoading && <LoadOverlay />}
         <div></div>
       </main>
     </div>
--- app/src/pages/projects.tsx
+++ app/src/pages/projects.tsx
@@ -5,13 +5,17 @@ import { Language, Project, SpineApi } from 'spine-api';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { SERVER_URL } from '../constants';
+import LoadOverlay from '../components/LoadOverlay';
 
 // Render project table only on client because we need window and document for correct dimension calculation
-const DynamicProjectListWithoutSSR = dynamic(() => import('../components/project-table/ProjectTable'), {
-  ssr: false,
-});
+const DynamicProjectListWithoutSSR = dynamic(
+  () => import('../components/project-table/ProjectTable'),
+  {
+    ssr: false,
+  },
+);
 
-const ProjectsPage: NextPage = (props) => {
+const ProjectsPage: NextPage = () => {
   const { i18n } = useTranslation();
   const [isLoading, setLoading] = useState(true);
   const [projects, setProjects] = useState<Project[]>([]);
@@ -34,30 +38,7 @@ const ProjectsPage: NextPage = (props) => {
       </Head>
 
       <main className="h-full">
-        {isLoading && (
-          <div className="absolute inset-0 flex items-center justify-center z-50 bg-black bg-opacity-80">
-            <svg
-              className="animate-spin h-12 w-12 text-accent-light"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-          </div>
-        )}
+        {isLoading && <LoadOverlay />}
         <DynamicProjectListWithoutSSR projects={projects} />
       </main>
     </div>
